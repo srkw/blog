c0b10a7989c369277ec6d3f8740f8e2bb1ec11a2
srkw <shirokawa.yuta@gmail.com>
2022-05-07 16:51:08 +0900

    add emotion

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,20 @@
 import type { NextPage } from 'next'
+import css from '@emotion/react'
 import { fetchBlogContents } from '../src/lib/api'
+import { Layout } from '../src/components/shared/Layout'
 
 const Home: NextPage<any> = ({ blogContents }) => {
   return (
-    <div>
-      srkw
+    <Layout>
       <div className={'blogContents'}>
         {blogContents.map((content: any) => <div key={content.id} dangerouslySetInnerHTML={{ __html: content.content }} />)}
       </div>
-    </div>
+    </Layout>
   )
 }
 
 export const getStaticProps = async () => {
   const contents = await fetchBlogContents().then(res => res.json()).then(result => result.contents)
-
-  console.log(contents)
 
   return { props: { blogContents: contents } }
 }
